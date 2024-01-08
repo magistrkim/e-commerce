16061e541efab791057b0112e65ee81f07ba5549
Nataliya Kim <magistr1982@gmail.com>
2024-01-08 10:55:11 +0200

    Create useFilter hook

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -1,7 +1,10 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
 import classes from './index.module.scss'
 import { Category, Media } from '../../../../payload/payload-types'
+import { useFilter } from '../../../_providers/Filter'
 
 type CategoryCardProps = {
   category: Category
@@ -9,11 +12,13 @@ type CategoryCardProps = {
 
 const CategoryCard = ({ category }: CategoryCardProps) => {
   const media = category.media as Media
+  const { setCategoryFilters } = useFilter()
   return (
     <Link
       href="/products"
       className={classes.card}
       style={{ backgroundImage: `url(${media.url})` }}
+      onClick={() => setCategoryFilters([category.id])}
     >
       <p className={classes.title}>{category.title}</p>
     </Link>
