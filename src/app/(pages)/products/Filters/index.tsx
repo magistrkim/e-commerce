3cf98c4f85fdc1961d657a47b6f3135be3f1d6ab
Nataliya Kim <magistr1982@gmail.com>
2024-01-10 10:01:17 +0200

    Add Checkbox component to Products page

--- src/app/(pages)/products/Filters/index.tsx
+++ src/app/(pages)/products/Filters/index.tsx
@@ -1,7 +1,40 @@
+'use client'
+
 import React from 'react'
 
-const Filters = () => {
-  return <div>Filters</div>
+import classes from './index.module.scss'
+import { useFilter } from '../../../_providers/Filter'
+import { Category } from '../../../../payload/payload-types'
+import { Checkbox } from '../../../_components/Checkbox'
+import { HR } from '../../../_components/HR'
+
+const Filters = ({ categories }: { categories: Category[] }) => {
+  const { categoryFilters, setCategoryFilters, sort, setSort } = useFilter()
+
+  const handleCategories = (categoryId: string) => {}
+  return (
+    <div className={classes.filters}>
+      <div>
+        <h6 className={classes.title}>Product Categories</h6>
+        <div className={classes.categories}>
+          {categories.map(category => {
+            const isSelected = categoryFilters.includes(category.id)
+            return (
+              <Checkbox
+                key={category.id}
+                label={category.title}
+                value={category.id}
+                isSelected={isSelected}
+                onClickHandler={handleCategories}
+              />
+            )
+          })}
+        </div>
+        <HR className={classes.hr} />
+        <h6 className={classes.title}>Sort by</h6>
+      </div>
+    </div>
+  )
 }
 
 export default Filters
--- src/app/_components/CollectionArchive/index.tsx
+++ src/app/_components/CollectionArchive/index.tsx
@@ -11,6 +11,7 @@ import { PageRange } from '../PageRange'
 import { Pagination } from '../Pagination'
 
 import classes from './index.module.scss'
+import { useFilter } from '../../_providers/Filter'
 
 type Result = {
   docs: Product[]
@@ -38,8 +39,8 @@ export type Props = {
 }
 
 export const CollectionArchive: React.FC<Props> = props => {
+  const { categoryFilters, sort } = useFilter()
   const {
-    categories: catsFromProps,
     className,
     limit = 10,
     onResultChange,
@@ -49,7 +50,6 @@ export const CollectionArchive: React.FC<Props> = props => {
     relationTo,
     selectedDocs,
     showPageRange,
-    sort = '-createdAt',
   } = props
 
   const [results, setResults] = useState<Result>({
@@ -75,7 +75,7 @@ export const CollectionArchive: React.FC<Props> = props => {
   const isRequesting = useRef(false)
   const [page, setPage] = useState(1)
 
-  const categories = (catsFromProps || []).map(cat => cat.id).join(',')
+  const categories = (categoryFilters || []).map((cat: string) => cat).join(',')
 
   const scrollToRef = useCallback(() => {
     const { current } = scrollRef
@@ -114,10 +114,10 @@ export const CollectionArchive: React.FC<Props> = props => {
           page,
           sort,
           where: {
-            ...(categories
+            ...(categoryFilters
               ? {
                   categories: {
-                    in: categories,
+                    in: categoryFilters,
                   },
                 }
               : {}),
@@ -160,7 +160,7 @@ export const CollectionArchive: React.FC<Props> = props => {
     return () => {
       if (timer) clearTimeout(timer)
     }
-  }, [page, categories, relationTo, onResultChange, sort, limit, populateBy])
+  }, [page, categoryFilters, relationTo, onResultChange, sort, limit, populateBy])
 
   return (
     <div className={[classes.collectionArchive, className].filter(Boolean).join(' ')}>
