4cb03f4872e3c23e4776e50b7554550fd04cf1ea
Nataliya Kim <magistr1982@gmail.com>
2024-01-13 15:38:34 +0200

    Add CartItem UI

--- src/app/(pages)/cart/CartItem/index.tsx
+++ src/app/(pages)/cart/CartItem/index.tsx
@@ -1,7 +1,14 @@
 'use client'
+
 import React, { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
 
 import classes from './index.module.scss'
+
+import { Media } from '../../../_components/Media'
+import { RemoveFromCartButton } from '../../../_components/RemoveFromCartButton'
+import { Price } from '../../../_components/Price'
 
 const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
   const [quantity, setQuantity] = useState(qty)
@@ -10,7 +17,48 @@ const CartItem = ({ product, title, metaImage, qty, addItemToCart }) => {
   const enterQty = () => {}
   return (
     <li className={classes.item} key={title}>
-      <h6>{title}</h6>
+      <Link href={`/products/${product.slug}`} className={classes.mediaWrapper}>
+        {!metaImage && <span>No image</span>}
+        {metaImage && typeof metaImage !== 'string' && (
+          <Media className={classes.media} imgClassName={classes.image} resource={metaImage} fill />
+        )}
+      </Link>
+      <div className={classes.itemDetails}>
+        <div className={classes.titleWrapper}>
+          <h6>{title}</h6>
+          <Price product={product} button={false} />
+        </div>
+        <div className={classes.quantity}>
+          <div className={classes.quantityBtn} onClick={decrementQty}>
+            <Image
+              src="/assets/icons/minus.svg"
+              alt="minus"
+              width={24}
+              height={24}
+              className={classes.qtnBt}
+            />
+          </div>
+          <input
+            type="text"
+            className={classes.quantityInput}
+            value={quantity}
+            onChange={enterQty}
+          />
+          <div className={classes.quantityBtn} onClick={incrementQty}>
+            <Image
+              src="/assets/icons/plus.svg"
+              alt="plus"
+              width={24}
+              height={24}
+              className={classes.qtnBt}
+            />
+          </div>
+        </div>
+      </div>
+      <div className={classes.subtotalWrapper}>
+        <Price product={product} button={false} quantity={quantity} />
+        <RemoveFromCartButton product={product} />
+      </div>
     </li>
   )
 }
